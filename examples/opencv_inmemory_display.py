bca1e5162f256adc4812d0b00eee14a998c06e4e
alkynee <alkynee@proton.me>
2022-01-14 13:03:39 +0000

    fix integration test

---
+++ examples/opencv_inmemory_display.py
@@ -0,0 +1,33 @@
+# Copyright 2020 Wearless Tech Inc All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import grpc
+from numpy.lib.function_base import append
+import video_streaming_pb2_grpc, video_streaming_pb2
+import argparse
+import cv2
+import numpy as np
+import time
+import os
+import threading
+import sys
+
+# create in-memory buffer gRPC request
+def gen_buffered_image_request(device_name, timestamp_from, timestamp_to):
+    """ Create an object to request a video frame """
+
+
+    req = video_streaming_pb2.VideoFrameBufferedRequest()
+    req.device_
