6537b531694ef43c4790a7d9561a35f8beecb478
alkynee <alkynee@proton.me>
2022-02-21 10:10:00 +0000

    Update info.json

--- server/utils/parser_utils.go
+++ server/utils/parser_utils.go
@@ -32,4 +32,42 @@ func StringPairsToVarPairs(stringPairs []string) []*models.VarPair {
 		for _, str := range stringPairs {
 			if strings.Contains(str, "=") {
 				pair := &models.VarPair{}
-				split := strings.Split(str,
+				split := strings.Split(str, "=")
+				name := split[0]
+				val := split[1]
+				pair.Name = name
+				pair.Value = val
+				varPairs = append(varPairs, pair)
+			}
+		}
+	}
+	return varPairs
+}
+
+// ImageTag to part splits the complete image tag to username, registry and version
+func ImageTagToParts(imageTag string) (string, string, string) {
+	// best effort return
+	if !strings.Contains(imageTag, "/") {
+		return imageTag, "", ""
+	}
+	splitUserRegistry := strings.Split(imageTag, "/")
+	if len(splitUserRegistry) == 2 {
+		dockerUser := splitUserRegistry[0]
+		part2 := splitUserRegistry[1]
+		registryVersion := strings.Split(part2, ":")
+		if len(registryVersion) == 2 {
+			dockerRepository := registryVersion[0]
+			dockerVersion := registryVersion[1]
+			return dockerUser, dockerRepository, dockerVersion
+		} else {
+			return imageTag, "", ""
+		}
+	}
+
+	return imageTag, "", ""
+}
+
+// combine image parts to complete image tag string
+func ImageTagPartToString(dockerUser, dockerRepository, dockerImageVersion string) string {
+	return dockerUser + "/" + dockerRepository + ":" + dockerImageVersion
+}
